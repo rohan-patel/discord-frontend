90624bc700d7db660100e129fd683d4597128a04
Rohan Patel <rohanpatel1531@gmail.com>
2023-05-03 12:16:45 +0530

    ..

--- src/ngrok.js
+++ src/ngrok.js
@@ -1,4 +1,4 @@
 export const apiUrl =
-  "https://0650-2402-3a80-1514-910-6567-9065-49e4-8a2e.ngrok-free.app";
+  "https://7bfb-2402-3a80-8b9-741c-cc1c-85b1-3b56-54e2.ngrok-free.app ";
 export const socketUrl =
-  "https://a224-2402-3a80-1514-910-6567-9065-49e4-8a2e.ngrok-free.app";
+  "https://639b-2402-3a80-8b9-741c-cc1c-85b1-3b56-54e2.ngrok-free.app";
